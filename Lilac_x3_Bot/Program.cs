f8646de921ba0e38572d33056dde7ff755da974b
Denis Kliem <denis_kliem@yahoo.de>
2020-12-14 12:38:23 +0100

    config.xml Load and Create Methodes added with handling exceptions. Tools class added for helpful methodes.

--- Lilac_x3_Bot/Program.cs
+++ Lilac_x3_Bot/Program.cs
@@ -2,6 +2,7 @@
 using Discord.WebSocket;
 using System;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Lilac_x3_Bot
 {
@@ -9,6 +10,8 @@ namespace Lilac_x3_Bot
     {
         // Member
         private DiscordSocketClient _client;
+        private Tools t = new Tools();
+        private XDocument configXML;
 
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
@@ -16,12 +19,15 @@ namespace Lilac_x3_Bot
 
         public async Task MainAsync()
         {
-            _client = new DiscordSocketClient();
+            // Load config.xml if doesn't exist create one. Both with exception handling.
+            this.configXML = this.LoadConfigXML();
 
-            _client.Log += Log;
+            this._client = new DiscordSocketClient();
 
-            await _client.LoginAsync(TokenType.Bot, "Token Here");
-            await _client.StartAsync();
+            this._client.Log += Log;
+
+            //await _client.LoginAsync(TokenType.Bot, "Token Here");
+            //await _client.StartAsync();
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
@@ -32,5 +38,41 @@ namespace Lilac_x3_Bot
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
+
+        #region Load and Create Config Methodes
+        private XDocument LoadConfigXML()
+        {
+            try
+            {
+                XDocument configXML = XDocument.Load("config.xml");
+                return configXML;
+            }
+            catch (Exception)
+            {
+                this.CreateConfigXML();
+                return null;
+            }
+        }
+
+        private void CreateConfigXML()
+        {
+            try
+            {
+                XDocument config = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
+                                                    new XElement("Init",
+                                                        new XElement("Token", "Token Here")
+                                                    )
+                                                );
+                config.Save("config.xml");
+                Console.WriteLine("config.xml war nicht vorhanden und wurde erstellt.");
+                this.t.CWLTextColor("Bitte die config.xml anpassen!", ConsoleColor.Yellow);
+            }
+            catch (Exception e)
+            {
+                this.t.CWLTextColor("config.xml konnte nicht erstellt werden.\nMore Details:", ConsoleColor.Red);
+                Console.WriteLine(e);
+            }
+        }
+        #endregion
     }
 }
---
+++ Lilac_x3_Bot/Tools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lilac_x3_Bot
+{
+    public class Tools
+    {
+        public void CWLTextColor(string text, ConsoleColor textColor)
+        {
+            Console.ForegroundColor = textColor;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
