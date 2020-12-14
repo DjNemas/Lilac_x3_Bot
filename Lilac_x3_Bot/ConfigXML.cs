2c21ad23778d5664246072577ac68729dd84eb76
Denis Kliem <denis_kliem@yahoo.de>
2020-12-14 16:07:32 +0100

    Bot is starting. Added more Exeption and Crash Handling to be sure.

--- Lilac_x3_Bot/ConfigXML.cs
+++ Lilac_x3_Bot/ConfigXML.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace Lilac_x3_Bot
@@ -7,24 +8,34 @@ namespace Lilac_x3_Bot
     {
         // Member
         private Tools t = new Tools();
-        private string configPath = "config.xml";
-
+        XDocument configXML;
+        static private string configFolder = @"E:\Visual Studio Projekte\dev\configs\";
+        static private string configFileName = "config.xml";
+        private string configPath = configFolder + configFileName;
+       
         public XDocument LoadConfigXML()
         {
             try
             {
-                XDocument configXML = XDocument.Load(configPath);
+                configXML = XDocument.Load(configPath);
                 return configXML;
             }
             catch (Exception)
             {
-                this.CreateConfigXML();
-
-                return null;
+                bool create = this.CreateConfigXML();
+                if (create == true)
+                {
+                    configXML = XDocument.Load(configPath);
+                    return configXML;
+                }
+                else
+                {
+                    return null;
+                }  
             }
         }
 
-        private void CreateConfigXML()
+        private bool CreateConfigXML()
         {
             try
             {
@@ -36,13 +47,34 @@ namespace Lilac_x3_Bot
                 config.Save(configPath);
                 Console.WriteLine("config.xml war nicht vorhanden und wurde erstellt.");
                 this.t.CWLTextColor("Bitte die config.xml anpassen!", ConsoleColor.Yellow);
+                return true;
             }
             catch (Exception e)
             {
                 this.t.CWLTextColor("config.xml konnte nicht erstellt werden.\nMore Details:", ConsoleColor.Red);
                 Console.WriteLine(e);
+                return false;
             }
         }
 
+        public string GetToken(XDocument configFile)
+        {
+            var token = from config in configFile.Descendants("Init")
+                        select config;
+            try
+            {
+                string tmp = "";
+                foreach (var item in token)
+                {
+                    tmp = item.Element("Token").Value;
+                }
+                return tmp;
+            }
+            catch (Exception)
+            {
+                t.CWLTextColor("Can't get Token from config.xml", ConsoleColor.Red);
+                return null;
+            }
+        }
     }
 }
--- Lilac_x3_Bot/Program.cs
+++ Lilac_x3_Bot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -10,6 +11,7 @@ namespace Lilac_x3_Bot
     {
         // Member
         private DiscordSocketClient _client;
+        private Tools t = new Tools();
         private ConfigXML configClass = new ConfigXML();
         private XDocument configXML;
 
@@ -19,15 +21,22 @@ namespace Lilac_x3_Bot
 
         public async Task MainAsync()
         {
+
             // Load config.xml if doesn't exist create one. Both with exception handling.
             this.configXML = this.configClass.LoadConfigXML();
-
+            
             this._client = new DiscordSocketClient();
 
             this._client.Log += Log;
-
-            //await _client.LoginAsync(TokenType.Bot, "Token Here");
-            //await _client.StartAsync();
+            if (this.configXML != null)
+            {
+                await _client.LoginAsync(TokenType.Bot, this.configClass.GetToken(configXML));
+            }
+            else
+            {
+                CloseByInvalidToken();
+            }
+            await _client.StartAsync();
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
@@ -37,6 +46,25 @@ namespace Lilac_x3_Bot
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
+        }
+
+        private void CloseByInvalidToken()
+        {
+            t.CWLTextColor("Der Token ist Invalid, bitte überprüfe den Token!", ConsoleColor.Red);
+            t.CWLTextColor("Das Programm wird in 5 sec geschlossen!", ConsoleColor.Yellow);
+            for (int i = 5; i >= 0; i--)
+            {
+                if (i > 0)
+                {
+                    t.CWLTextColor(i.ToString(), ConsoleColor.Yellow);
+                }
+                else if (i == 0)
+                {
+                    t.CWLTextColor("Programm is closing", ConsoleColor.Yellow);
+                }
+                Thread.Sleep(1000);
+            }
+            System.Environment.Exit(1);
         }
     }
 }
