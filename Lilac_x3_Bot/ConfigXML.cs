da09f8f9e02c067b93483125cbe20fa6ac117cd8
Denis Kliem <denis_kliem@yahoo.de>
2020-12-14 13:24:25 +0100

    ConfigXML class createted for better readable code

---
+++ Lilac_x3_Bot/ConfigXML.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Lilac_x3_Bot
+{
+    public class ConfigXML
+    {
+        // Member
+        private Tools t = new Tools();
+        private string configPath = "config.xml";
+
+        public XDocument LoadConfigXML()
+        {
+            try
+            {
+                XDocument configXML = XDocument.Load(configPath);
+                return configXML;
+            }
+            catch (Exception)
+            {
+                this.CreateConfigXML();
+
+                return null;
+            }
+        }
+
+        private void CreateConfigXML()
+        {
+            try
+            {
+                XDocument config = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
+                                                    new XElement("Init",
+                                                        new XElement("Token", "Token Here")
+                                                    )
+                                                );
+                config.Save(configPath);
+                Console.WriteLine("config.xml war nicht vorhanden und wurde erstellt.");
+                this.t.CWLTextColor("Bitte die config.xml anpassen!", ConsoleColor.Yellow);
+            }
+            catch (Exception e)
+            {
+                this.t.CWLTextColor("config.xml konnte nicht erstellt werden.\nMore Details:", ConsoleColor.Red);
+                Console.WriteLine(e);
+            }
+        }
+
+    }
+}
--- Lilac_x3_Bot/Program.cs
+++ Lilac_x3_Bot/Program.cs
@@ -10,7 +10,7 @@ namespace Lilac_x3_Bot
     {
         // Member
         private DiscordSocketClient _client;
-        private Tools t = new Tools();
+        private ConfigXML configClass = new ConfigXML();
         private XDocument configXML;
 
         public static void Main(string[] args)
@@ -20,7 +20,7 @@ namespace Lilac_x3_Bot
         public async Task MainAsync()
         {
             // Load config.xml if doesn't exist create one. Both with exception handling.
-            this.configXML = this.LoadConfigXML();
+            this.configXML = this.configClass.LoadConfigXML();
 
             this._client = new DiscordSocketClient();
 
@@ -38,41 +38,5 @@ namespace Lilac_x3_Bot
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
-
-        #region Load and Create Config Methodes
-        private XDocument LoadConfigXML()
-        {
-            try
-            {
-                XDocument configXML = XDocument.Load("config.xml");
-                return configXML;
-            }
-            catch (Exception)
-            {
-                this.CreateConfigXML();
-                return null;
-            }
-        }
-
-        private void CreateConfigXML()
-        {
-            try
-            {
-                XDocument config = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
-                                                    new XElement("Init",
-                                                        new XElement("Token", "Token Here")
-                                                    )
-                                                );
-                config.Save("config.xml");
-                Console.WriteLine("config.xml war nicht vorhanden und wurde erstellt.");
-                this.t.CWLTextColor("Bitte die config.xml anpassen!", ConsoleColor.Yellow);
-            }
-            catch (Exception e)
-            {
-                this.t.CWLTextColor("config.xml konnte nicht erstellt werden.\nMore Details:", ConsoleColor.Red);
-                Console.WriteLine(e);
-            }
-        }
-        #endregion
     }
 }
